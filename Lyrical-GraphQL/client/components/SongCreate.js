210b2de02e65e1f281a5e7f024f13c361e3bc2b5
adamdubicki <adam@freshworks.io>
2018-07-26 20:10:58 -0700

    add song, delete song

--- Lyrical-GraphQL/client/components/SongCreate.js
+++ Lyrical-GraphQL/client/components/SongCreate.js
@@ -1,22 +1,27 @@
 import React, { Component } from 'react';
 import gql from 'graphql-tag';
+import { graphql } from 'react-apollo';
+import { Link, hashHistory } from 'react-router';
+import query from '../queries/fetchSongs';
 
 class SongCreate extends Component {
     constructor(props){
         super(props);
-
         this.state = { title: '' }
     }
 
     onSubmit(event){
         event.preventDefault();
-
-
+        this.props.mutate({
+            variables: { title: this.state.title },
+            refetchQueries: [{ query }]
+        }).then(() => hashHistory.push('/'));
     }
 
     render() {
         return (
             <div>
+                <Link to="/">Back</Link>
                 <h3>Create a new song</h3>
                 <form onSubmit={this.onSubmit.bind(this)}>
                     <label>Song Title:</label>
@@ -30,10 +35,13 @@ class SongCreate extends Component {
     }
 }
 
-// const mutation = gql`
-//     mutation {
-//         addSong(title: ${this.state})
-//     }
-// `;
+const mutation = gql`
+    mutation AddSong($title: String){
+        addSong(title: $title){
+            id
+            title
+        }
+    }
+`;
 
-export default SongCreate;
+export default graphql(mutation)(SongCreate);
